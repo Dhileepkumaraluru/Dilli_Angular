d672252b5ce9803e631b09b0216e73c64de085fa
sadhasiva28 <sadhasivadilli001@gmail.com>
2023-04-18 15:27:13 +0530

    modifycode

--- Day8-17-04/tasktoday/src/app/topbar/topbar.component.ts
+++ Day8-17-04/tasktoday/src/app/topbar/topbar.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog'; // added import statement for MatDialog
+import { LoginComponent } from '../popup/login/login.component';
 
 @Component({
   selector: 'app-topbar',
@@ -6,5 +8,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./topbar.component.css']
 })
 export class TopbarComponent {
+  constructor(public dialog: MatDialog) {}
+  
+  openDialog() {
+    this.dialog.open(LoginComponent);
+  }
 
 }
